5efe11710abb2764c3bdbd74ec43d462fb330d1f
Ivancov Alexander <ivancovae@kodeks.ru>
2024-09-20 18:19:30 +0300

    fix styles

--- src/Component/ListElement/ListElement.tsx
+++ src/Component/ListElement/ListElement.tsx
@@ -6,54 +6,71 @@ import { createField, IField } from "../../App";
 import './ListElement.scss';
 
 type Props = {
-        element: IField, 
-        onRemove: (id: string) => void, 
-        onEnterEdit: (id: string) => void, 
-        onExitEdit: (newElem: IField) => void, 
-        isEdit: boolean  
+    element: IField,
+    onRemove: (id: string) => void,
+    onEnterEdit: (id: string) => void,
+    onExitEdit: (newElem: IField) => void,
+    isEdit: boolean
 }
 
 export const ListElement = ({
-    element, 
-    onRemove, 
-    onEnterEdit, 
-    onExitEdit, 
+    element,
+    onRemove,
+    onEnterEdit,
+    onExitEdit,
     isEdit
-    }:Props): JSX.Element => {
+}: Props): JSX.Element => {
 
 
-    const [changedFirstNameValue, setFirstNameValue] = useState(element.firstname);
-    const [changedSecondNameValue, setSecondNameValue] = useState(element.secondname);
-    const [changedBirthdayValue, setBirthdayValue] = useState(element.birthday.toLocaleDateString("ru-RU"));
+    const [changedFirstNameValue, setFirstNameValue] = useState<string>(element.firstname);
+    const [changedSecondNameValue, setSecondNameValue] = useState<string>(element.secondname);
+    const [changedBirthdayValue, setBirthdayValue] = useState<string>(element.birthday.toLocaleDateString("ru-RU"));
 
     const handleChange = (type: any, value: any) => {
-        if (''+type == 'firstName') {
+        if ('' + type == 'firstName') {
             setFirstNameValue(value);
-        } else if (''+type == 'secondName') {
+        } else if ('' + type == 'secondName') {
             setSecondNameValue(value);
-        } else if (''+type == 'birthday') {
+        } else if ('' + type == 'birthday') {
             setBirthdayValue(value);
         }
     }
 
     const getEditElement = () => {
         return (
-        <div className="editElements">
-            <input value={changedFirstNameValue} onChange={(event)=>{handleChange('firstName', event.target.value)}} />
-            <input value={changedSecondNameValue} onChange={(event)=>{handleChange('secondName', event.target.value)}} />
-            <input value={changedBirthdayValue} onChange={(event)=>{handleChange('birthday', event.target.value)}} />
-        </div>
+            <div className="editElements">
+                <input 
+                    value={changedFirstNameValue} 
+                    onChange={(event) => { handleChange('firstName', event.target.value) }} 
+                />
+                <input 
+                    value={changedSecondNameValue} 
+                    onChange={(event) => { handleChange('secondName', event.target.value) }}
+                />
+                <input 
+                    value={changedBirthdayValue} 
+                    onChange={(event) => { handleChange('birthday', event.target.value) }} 
+                />
+            </div>
         );
     }
 
     const getEditButton = () => {
         if (isEdit) {
-                return (
-                    <button type="button" onClick={() => onExitEdit(createField(changedFirstNameValue, changedSecondNameValue, new Date(changedBirthdayValue), element.id))}>{`Выйти из редактирования`}</button>
+            return (
+                <button 
+                    type="button" 
+                    onClick={() => {
+                        const value = createField(changedFirstNameValue, changedSecondNameValue, new Date(changedBirthdayValue), element.id);
+                        onExitEdit(value);
+                    }}
+                >
+                    {`Выйти из редактирования`}
+                </button>
             );
         } else {
             return (
-                <button type="button" onClick={() => onEnterEdit(element.id)}>{`Редактирование`}</button> 
+                <button type="button" onClick={() => onEnterEdit(element.id)}>{`Редактирование`}</button>
             );
         }
     }
@@ -62,20 +79,20 @@ export const ListElement = ({
         <li className={'listElement'}>
             <div>
                 {
-                (!isEdit) ?
-                    <div className="showElements">
-                    <label className="showElement">{`${changedFirstNameValue}`}</label>
-                    <label className="showElement">{`${changedSecondNameValue}`}</label>
-                    <label className="showElement">{`${changedBirthdayValue}`}</label>
-                    </div> :
-                    getEditElement()
+                    (!isEdit) ?
+                        <div className="showElements">
+                            <label className="showElement">{`${changedFirstNameValue}`}</label>
+                            <label className="showElement">{`${changedSecondNameValue}`}</label>
+                            <label className="showElement">{`${changedBirthdayValue}`}</label>
+                        </div> :
+                        getEditElement()
                 }
             </div>
             <div>
                 {(getEditButton())}
                 <button type="button" onClick={() => onRemove(element.id)}>{`Удалить`}</button>
             </div>
-            
+
         </li>
     );
 }
